d7ad92fd3315e728fc84c83ff925c1a14e2c53e6
Pablo Ferreiro <40209379+pabloferre@users.noreply.github.com>
2023-08-20 20:52:26 -0400

    class cluster

--- ETL/E_cluster_class.py
+++ ETL/E_cluster_class.py
@@ -7,7 +7,6 @@ import time
 from openai.error import APIError
 path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 os.chdir(path)
-print(path)
 from lib.general_module import get_conn, get_embeddings, ensure_columns, categorize, execute_sql
 from lib.openai_module import classify_field
 
@@ -20,47 +19,43 @@ openai.organization = OPENAI_ORG_ID
 
 
 
-df =  pd.read_excel(path_to_drive + r'cluster_117.xlsx')#dataframe with idea name, idea_id, goal_id, name and embedded column with a mean of all the embedded columns of the idea
-df.rename(columns={'solution_1':'description'}, inplace=True)
+###########################AUXILIARY FUNCTIONS#################################################
 
-
-def classify_cluster(df:pd.DataFrame)->dict:
-    """
-    Classify a cluster of ideas with its descriptions and name the cluster, the ideas and give a short description of each idea.
+def create_message(df:pd.DataFrame)->str:
     
-    Args:
-        df (pd.DataFrame): Filtered dataframe of a cluster of ideas with its descritions
-
-    Returns:
-        dict: chat gpt answer for naming the cluster, giving a name to the idea and a short description
-    """
-    
-    base_message = f"I have the following cluster of ideas with it name and description. Need to resume each idea\
-        in one word, then a small description of no more than 25 words, and finally a name for the whole cluster. \
-        Give the answer in the same order of the list. Each idea is wrapped between [ ] This is the cluster: \n"
+    base_message = """I have the following cluster of ideas with it name and description. \
+    return only a python dictionary. The dictionary should be as the \
+    following json:  { 'cluster_name': 'you_asnwer for the cluster name in no more than 4 words', \
+    'cluster_description': 'you_asnwer for the cluster name in no more than 4 words', '1': {'name': \
+    'your answer for the idea number 1 name', 'description': 'you answer for idea number 1 description'} } \
+    This is the cluster: \n"""
         
     cluster_text = ''
     
-    end_message = "\n return only a dictionary ready to be parsed in python in spanish. The dictionary should have a key cluster_name \
-    and a value with your answer, a key with cluster_description and a value your answer. Then a key with the number of the idea, and for value \
-    a dictionary with key name and value with your answer for the idea name and another key description with you answer for the idea description."
+    end_message = "Here is an example: {'cluster_name': 'Oferta turística y comercial', 'cluster_description': \
+    'Ideas para mejorar la oferta turística y comercial en las estaciones', '214': {'name': 'Módulos comerciales en plazoleta', \
+    'description': 'Implementar módulos para guías turísticos en la plazoleta de la estación San Javier.'} } \
+    This would be an error: {'cluster_name': 'Oferta turística', 'cluster_description': 'musica', 'ideas': {'214': 'name': 'Nombre', \
+        'description': 'Descripcion'}. Rembember, ONLY return the dictionary"
     
     for i, row in df.iterrows():
-        text = '[Idea ' + str(i) + ': Name: ' + str(df.loc[i,'name']) + '| Description: ' + str(df.loc[i,'description']) + "]\n"
+        text = '[Idea number ' + str(i) + ': Name: ' + str(df.loc[i,'name']) + '| Description: ' + str(df.loc[i,'description']) + "]\n"
+        if len(text) >1000:
+            text = '[Idea number ' + str(i) + ': Name: ' + str(df.loc[i,'name']) + '| Description: ' + str(df.loc[i,'description'])[:500] + "]\n"
         cluster_text += text
-        
-    message = base_message + cluster_text + end_message
     
-    
+    return clean_text(base_message + cluster_text + end_message)
+
+
+def classify_cluster(df:pd.DataFrame)->dict:
+    message = create_message(df)
     response = openai.ChatCompletion.create(
             model ="gpt-3.5-turbo",
             messages = [{"role": "system", 
                           "content": "You are a helpful assistant."}, 
                          {"role": "user", "content": message}],
-            max_tokens=1000,
             temperature=0,
     )
-
     return ast.literal_eval(response.choices[0].message['content'])
 
 
@@ -76,13 +71,71 @@ def fill_cluster(df:pd.DataFrame, answer:dict)->pd.DataFrame:
     Returns:
         pd.DataFrame: dataframe with the answers of the cluster classification
     """
-    df.loc[:,'cluster_name'] = answer['cluster_name']
-    df.loc[:,'cluster_description'] = answer['cluster_description']
+    df['cluster_name'] = answer['cluster_name']
+    df['cluster_description'] = answer['cluster_description']
     for i, row in df.iterrows():
-        df.loc[i,'idea_name'] = answer[str(i)]['name']
-        df.loc[i,'idea_description'] = answer[str(i)]['description']
+        df.at[i,'idea_name'] = answer[str(i)]['name']
+        df.at[i,'idea_description'] = answer[str(i)]['description']
     return df
 
 
-a = classify_cluster(df.loc[df.loc[:,'cluster']==0])
-b = fill_cluster(df.loc[df.loc[:,'cluster']==0], a)
+def chunk_dataframe(df, max_rows=5): 
+    """
+    Yields chunks of the dataframe with up to max_rows rows.
+    """
+    for i in range(0, len(df), max_rows):
+        yield df.iloc[i:i + max_rows]
+
+def classify_clusters_from_chunks(chunks):
+    """
+    Process dataframe chunks and classify them.
+    """
+    all_answers = {}
+    
+    for chunk in chunks:
+        chunk =pd.DataFrame(chunk)
+        answer = classify_cluster(chunk)
+        all_answers.update(answer)
+        
+    return all_answers
+
+def clean_text(text):
+    # Remove extra lines and strip spaces from each line
+    lines = [line.strip() for line in text.splitlines() if line.strip()]
+    # Join the cleaned lines
+    cleaned_text = ' '.join(lines)
+    # Remove extra spaces
+    cleaned_text = ' '.join(cleaned_text.split())
+    return cleaned_text
+
+
+df =  pd.read_excel(path_to_drive + r'cluster_117.xlsx')#dataframe with idea name, idea_id, goal_id, name and embedded column with a mean of all the embedded columns of the idea
+df.rename(columns={'solution_1':'description', 'combined_labels':'cluster'}, inplace=True)
+df.sort_values('cluster', inplace=True)
+df = df.reset_index(drop=True)
+
+
+df_ = pd.DataFrame()
+n = 0
+parsed_clusters = [i for i in range(29)]
+for i in df['cluster'].unique():
+    if i in parsed_clusters:
+        continue
+    print(n, i)
+    if n == 30:
+        break
+    
+    chunks = list(chunk_dataframe(df.loc[df.loc[:,'cluster']==i]))
+
+    # Classify each chunk
+
+    answers = classify_clusters_from_chunks(chunks)
+
+
+    # Fill dataframe with answers
+    df_stg = fill_cluster(df.loc[df.loc[:,'cluster']==i], answers)
+    
+
+    df_ = pd.concat([df_,df_stg])
+    n += 1
+    time.sleep(1)
--- dw_sql/bridge_goal_idea.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS innk_dw_dev.public.bridge_goal_idea(
-    id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL UNIQUE,
-    goal_id INT,
-    CONSTRAINT fk_id_goal FOREIGN KEY (goal_id) REFERENCES dim_goals(id),
-    id_company INT,
-    CONSTRAINT fk_id_company FOREIGN KEY (id_company) REFERENCES dim_companies(id),
-    idea_id INT,
-    CONSTRAINT fK_id_idea FOREIGN KEY (id_idea) REFERENCES dim_ideas(id),
-)
